e8a14d4dd62b215183da38235cb9d9a11f75134f
samfeldman <samantha.ariel.feldman@gmail.com>
2014-10-22 12:02:15 -0400

    created .js and .html files, created form and divs for clock, created initial javascript for millisecond countdown

---
+++ groupproject.js
@@ -0,0 +1,76 @@
+//username, password
+
+// write html with elements of the clock
+// two rectangles one inside the other
+// center rectangle has a div with the clock numbers inside it
+
+// array with the time
+// function with if: when user submits form the countdown starts
+// 
+
+//
+
+
+
+
+
+// zero triggers alert with ACCESS DENIED PLEASE CALL FOR HELP
+
+$(document).ready(function() {
+
+	// function checkPass() {
+		
+	// 	if ($("#password").val() == "") {
+	// 		alert("Please fill in a password!");
+	// 		return false
+	// 	} else if ($("#password").val() !== "12345") {
+	// 		alert("Password incorrect, try again.");
+	// 		return false
+	// 	} else {
+	// 		return true
+	// 	}
+
+	// }
+
+	// function checkUsername() {
+
+	// 	if ($("#username").val() == "") {
+	// 		alert("Please fill in a username!");
+	// 		return false
+	// 	} else {
+	// 		return true
+	// 	}
+
+	// }
+
+	// $("form").submit(function(){
+		
+	// 	if(checkPass() && checkUsername()){
+	// 		alert("The form has been submitted.");
+	// 	}
+	
+	// 	return false
+
+	// });
+		
+			
+	//SET DELAY 
+
+			var i = 99
+			
+			function milliseconds() {
+				
+				setTimeout(function() {
+					
+					$(".milliseconds").html(i);
+					i--;
+					if (i < 100 && i >= 0) {
+						milliseconds();
+					}
+				}, 100)
+				
+			}
+
+			milliseconds();
+
+});
